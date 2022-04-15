7ca2e98adbe729fd29e43a8296a3a4c3baa7ebe7
Vanessa Reese <88684757+Vinesse-Nevertheless@users.noreply.github.com>
2022-04-15 13:53:18 -0400

    Update Movie.java

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/main/models/Movie.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/main/models/Movie.java
@@ -4,7 +4,6 @@ import java.util.Objects;
 
 public class Movie {
     private String name;
-  //  private String format;
     private double rating;
     private double sellingPrice;
     private double rentalPrice;
